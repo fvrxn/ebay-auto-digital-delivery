9b82e881d59a718831894289acd4080f01ee7d1d
Farin Marscholl <48458349+fvrxn@users.noreply.github.com>
2024-12-21 02:32:16 +0100

    Update telegramAPI.py

--- utils/telegramAPI.py
+++ utils/telegramAPI.py
@@ -1,12 +1,19 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 
 # function to send a notification via telegram
-def send_telegram_notification(api_token, chat_id, message):
-    url = f"https://api.telegram.org/bot{api_token}/sendMessage"
+def send_telegram_notification(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage"
     params = {
-        "chat_id": chat_id,
+        "chat_id": TELEGRAM_CHAT_ID,
         "text": message,
     }
     response = requests.get(url, params=params)
